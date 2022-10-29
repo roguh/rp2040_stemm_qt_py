b8bc3bff32f473e54ad6ca712a11de86571d73ff
Hugo O. Rivera <hugo@roguh.com>
2022-10-29 16:47:04 -0600

    Port Pico_ePaper-2.13-B.py to RP2040 Pico Zero

--- Makefile
+++ Makefile
@@ -7,13 +7,18 @@ BOOT_DEST=${BASE_DEST}/RPI-RP2
 DEST:=${BASE_DEST}/CIRCUITPY
 MPY_CROSS_COMPILER=./deps/mpy-cross.static-amd64-linux-8.0.0-beta.3-29-ga0f389e28
 # TODO use the circ... dep manager
-LIBS=./adafruit-circuitpython-bundle-8.x-mpy-20221029/lib/{adafruit_pixelbuf.mpy,neopixel.mpy} \
-		 ./adafruit-circuitpython-framebuf-8.x-mpy-1.4.14/lib/adafruit_framebuf.mpy
+LIBS=./deps/adafruit-circuitpython-bundle-8.x-mpy-20221029/lib/{adafruit_pixelbuf.mpy,neopixel.mpy} \
+		 ./deps/adafruit-circuitpython-framebuf-8.x-mpy-1.4.14/lib/adafruit_framebuf.mpy
 
-FIRMWARE=./adafruit-circuitpython-waveshare_rp2040_zero-en_US-8.0.0-beta.3.uf2
+FONTS=./deps/adafruit-circuitpython-framebuf-8.x-mpy-1.4.14/examples/font5x8.bin
+
+EPD=./epaper_2in13_b.py
+COMPILED_EPD=./epaper_2in13_b.mpy
+
+FIRMWARE=./deps/adafruit-circuitpython-waveshare_rp2040_zero-en_US-8.0.0-beta.3.uf2
 
 cp:
-	rm ${DEST}
+	rm ${DEST}/*.py
 	cp ${PYFILE} ${DEST}
 
 cp-compile:
@@ -21,6 +26,11 @@ cp-compile:
 	rm ${DEST}/code.py ${DEST}/${COMPILED_PYFILE}
 	cp main_for_compiled.py ${DEST}/code.py
 	cp ${COMPILED_PYFILE} ${DEST}
+
+compile-libraries:
+	${MPY_CROSS_COMPILER} ${EPD} -o ${COMPILED_EPD}
+	rm -f ${DEST}/${COMPILED_EPD}
+	cp ${COMPILED_EPD} ${DEST}
 
 dev:
 	watch -n1 \
@@ -48,8 +58,10 @@ install-python:
 	while [ ! -d ${DEST} ] ; do sleep 1 ; done
 	make install-libraries
 
-install-libraries:
+install-libraries: compile-libraries
 	@echo Installing libraries
+	cp ${FONTS} ${DEST}/
+	cp ${COMPILED_EPD} ${DEST}/lib
 	cp ${LIBS} ${DEST}/lib
 
 download-deps:
---
+++ epaper_2in13_b.py
@@ -0,0 +1,279 @@
+# *****************************************************************************
+# * | File        :	  epaper_2in13_b.py
+# * | Author      :   Waveshare team and Hugo O. Rivera
+# * | Function    :   Electronic paper driver
+# * | Info        :
+# *----------------
+# * | This version:   V0.1
+# * | Date        :   2022
+# # | Info        :   python library and demo
+# -----------------------------------------------------------------------------
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documnetation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to  whom the Software is
+# furished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS OR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+
+import time
+
+import adafruit_framebuf
+import board
+from adafruit_framebuf import FrameBuffer
+from busio import SPI
+from digitalio import DigitalInOut, Direction, Pull
+
+EPD_WIDTH = 104
+EPD_HEIGHT = 212
+
+# Either MHMSB or MVLSB
+PIXEL_FORMAT = adafruit_framebuf.MHMSB
+
+# Pico Zero: MOSI aka GPIO3 aka TX0 (transmit) for SPI protocol
+# Waveshare: DIN aka MOSI pin of SPI interface, data transmitted from Master to Slave.
+SPI_MOSI_PIN = board.GP3
+
+# Pico Zero: MISO aka GPIO4 aka RX0 (receive) for SPI protocol
+# Waveshare: DC aka Data/Command control pin (High: Data; Low: Command)
+# CircuitPython tutorial: MISO = main output, secondary input\u2019
+SPI_MISO_PIN = board.GP4
+
+# Pico Zero: RX aka GPIO5 aka CSn0 (chip select) for SPI protocol
+# Waveshare: CS aka Chip select pin of SPI interface, Low active
+# CircuitPython tutorial: most chips have a CS, or chip select, wire which is toggled to tell the chip that it should listen and respond to requests on the SPI bus. Each device requires its own unique CS line.
+SPI_CHIP_SELECT_PIN = board.GP5
+
+# Pico Zero: SCK aka GPIO6 aka SCK0 (SPI clock) for SPI protocol
+# Waveshare: CLK aka SCK pin of SPI interface, clock input
+# CircuitPython tutorial: most chips have a CS, or chip select, wire which is toggled to tell the chip that it should listen and respond to requests on the SPI bus. Each device requires its own unique CS line.
+SPI_CLOCK_PIN = board.GP6
+
+# Do the RST and BUSY pins just function as GPIO pins or is it part of the SPI protocol?
+
+# Pico Zero: SDA aka GPIO24 aka RXn1 (receive) for SPI protocol ??
+# Waveshare: RST aka Reset pin, low active
+EPAPER_RESET_PIN = board.GP24
+
+# Pico Zero: SCL aka GPIO25 aka CSn1 (clock select) for SPI protocol ??
+# Waveshare: BUSY aka Busy pin
+EPAPER_BUSY_PIN = board.GP25
+
+
+buffer_black = bytearray(EPD_HEIGHT * EPD_WIDTH // 8)
+imageblack = FrameBuffer(buffer_black, EPD_WIDTH, EPD_HEIGHT, PIXEL_FORMAT)
+# import epaper_2in13_b as e
+
+
+class EPD_2in13_B:
+    def __init__(self):
+        self.reset_pin = DigitalInOut(EPAPER_RESET_PIN)
+        self.reset_pin.direction = Direction.OUTPUT
+
+        self.busy_pin = DigitalInOut(EPAPER_BUSY_PIN)
+        self.busy_pin.direction = Direction.INPUT
+        self.busy_pin.pull = Pull.UP
+
+        self.cs_pin = DigitalInOut(SPI_CHIP_SELECT_PIN)
+        self.cs_pin.direction = Direction.OUTPUT
+
+        self.dc_pin = DigitalInOut(SPI_MISO_PIN)
+        self.dc_pin.direction = Direction.OUTPUT
+
+        self.width = EPD_WIDTH
+        self.height = EPD_HEIGHT
+
+        self.spi = SPI(SPI_CLOCK_PIN, MOSI=SPI_MOSI_PIN)
+
+        if not self.spi.try_lock():
+            raise RuntimeError("SPI device is locked")
+        self.spi.configure(baudrate=4_000_000)
+        self.spi.unlock()
+
+        self.buffer_black = bytearray(self.height * self.width // 8)
+        self.buffer_red = bytearray(self.height * self.width // 8)
+        self.imageblack = FrameBuffer(
+            self.buffer_black, self.width, self.height, PIXEL_FORMAT
+        )
+        self.imagered = FrameBuffer(
+            self.buffer_red, self.width, self.height, PIXEL_FORMAT
+        )
+        self.init()
+
+    def digital_write(self, pin, value):
+        pin.value = value
+
+    def digital_read(self, pin):
+        return pin.value
+
+    def delay_ms(self, delaytime):
+        time.sleep(delaytime / 1000)
+
+    def spi_writebyte(self, data):
+        if not self.spi.try_lock():
+            raise RuntimeError("SPI device is locked")
+        self.spi.write(bytearray(data))
+        self.spi.unlock()
+
+    def module_exit(self):
+        self.digital_write(self.reset_pin, 0)
+
+    # Hardware reset
+    def reset(self):
+        print("resetting epd")
+        self.digital_write(self.reset_pin, 1)
+        self.delay_ms(50)
+        self.digital_write(self.reset_pin, 0)
+        self.delay_ms(2)
+        self.digital_write(self.reset_pin, 1)
+        self.delay_ms(50)
+
+    def send_command(self, command):
+        # print("sending command", command)
+        self.digital_write(self.dc_pin, 0)
+        self.digital_write(self.cs_pin, 0)
+        self.spi_writebyte([command])
+        self.digital_write(self.cs_pin, 1)
+
+    def send_data(self, data):
+        # print("sending data", data)
+        self.digital_write(self.dc_pin, 1)
+        self.digital_write(self.cs_pin, 0)
+        self.spi_writebyte([data])
+        self.digital_write(self.cs_pin, 1)
+
+    def ReadBusy(self):
+        start_time = time.monotonic()
+        print("epd screen will be busy")
+        self.send_command(0x71)
+        while self.digital_read(self.busy_pin) == 0:
+            self.send_command(0x71)
+            self.delay_ms(10)
+        print("epd screen was busy for", time.monotonic() - start_time, "seconds")
+
+    def TurnOnDisplay(self):
+        self.send_command(0x12)
+        self.ReadBusy()
+
+    def init(self):
+        print("epd init")
+        self.reset()
+        self.send_command(0x04)
+        self.ReadBusy()  # waiting for the electronic paper IC to release the idle signal
+
+        self.send_command(0x00)  # panel setting
+        self.send_data(0x0F)  # LUT from OTP,128x296
+        self.send_data(
+            0x89
+        )  # Temperature sensor, boost and other related timing settings
+
+        self.send_command(0x61)  # resolution setting
+        self.send_data(0x68)
+        self.send_data(0x00)
+        self.send_data(0xD4)
+
+        self.send_command(0x50)  # VCOM AND DATA INTERVAL SETTING
+        self.send_data(0x77)  # WBmode:VBDF 17|D7 VBDW 97 VBDB 57
+        # WBRmode:VBDF F7 VBDW 77 VBDB 37  VBDR B7
+        return 0
+
+    def display(self):
+        self.send_command(0x10)
+        for j in range(0, self.height):
+            for i in range(0, int(self.width / 8)):
+                self.send_data(self.buffer_black[i + j * int(self.width / 8)])
+        self.send_command(0x13)
+        for j in range(0, self.height):
+            for i in range(0, int(self.width / 8)):
+                self.send_data(self.buffer_red[i + j * int(self.width / 8)])
+
+        self.TurnOnDisplay()
+
+    def Clear(self, fillblack=False, fillred=False):
+        if fillblack:
+            colorblack = 0
+        else:
+            colorblack = 0xFF
+
+        if fillred:
+            colorred = 0
+        else:
+            colorred = 0xFF
+
+        print("epd clearing")
+        self.send_command(0x10)
+        for j in range(0, self.height):
+            for i in range(0, int(self.width / 8)):
+                self.send_data(colorblack)
+        self.send_command(0x13)
+        for j in range(0, self.height):
+            for i in range(0, int(self.width / 8)):
+                self.send_data(colorred)
+
+        self.TurnOnDisplay()
+
+    def sleep(self):
+        self.send_command(0x50)
+        self.send_data(0xF7)
+        self.send_command(0x02)
+        self.ReadBusy()
+        self.send_command(0x07)  # DEEP_SLEEP
+        self.send_data(0xA5)  # check code
+
+        self.delay_ms(2000)
+        self.module_exit()
+
+
+def main():
+    print("DEMO Initializing and clearing screen")
+    epd = EPD_2in13_B()
+    epd.Clear(fillred=True)
+
+    print("DEMO Drawing text")
+    epd.imageblack.fill(0xFF)
+    epd.imagered.fill(0xFF)
+    epd.imageblack.text("Waveshare", 0, 10, 0x00)
+    epd.imagered.text("ePaper-2.13", 0, 25, 0x00)
+    epd.imageblack.text("RPi Pico Zero", 0, 40, 0x00)
+    epd.imagered.text("Hello World", 0, 55, 0x00)
+    epd.display()
+    epd.delay_ms(2000)
+
+    print("DEMO Drawing lines")
+    epd.imagered.vline(10, 90, 40, 0x00)
+    epd.imagered.vline(90, 90, 40, 0x00)
+    epd.imageblack.hline(10, 90, 80, 0x00)
+    epd.imageblack.hline(10, 130, 80, 0x00)
+    epd.imagered.line(10, 90, 90, 130, 0x00)
+    epd.imageblack.line(90, 90, 10, 130, 0x00)
+    epd.display()
+    epd.delay_ms(3000)
+
+    epd.imageblack.rect(10, 150, 40, 40, 0x00)
+    epd.imagered.fill_rect(60, 150, 40, 40, 0x00)
+    epd.display()
+    epd.delay_ms(2000)
+
+    epd.Clear()
+    epd.delay_ms(3000)
+
+    print("DEMO putting screen into low-voltage sleep mode")
+    print(
+        "DEMO WARNING: If the screen is not in sleep mode or powered off, it will remain in a high voltage state for a long time, which will damage the e-Paper and cannot be repaired!"
+    )
+    epd.sleep()
+
+
+if __name__ == "__main__":
+    main()
